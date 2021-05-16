67c5e9df4d5147ca214a5488accc5b3fd5014bd2
Álvaro Berzosa <56123570+alberzos@users.noreply.github.com>
2021-05-16 14:05:13 +0200

    1.1.5 Final Version
    Con Main

---
+++ Main.m
@@ -0,0 +1,11 @@
+classdef Main
+    
+    methods
+        function app = Main()
+            clc; clear;
+            clear all;
+            Controller.getInstance().execute(Events.GUI_INICIO, nan);
+        end
+
+    end
+end
